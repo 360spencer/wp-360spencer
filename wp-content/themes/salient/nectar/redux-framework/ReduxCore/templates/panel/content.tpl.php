ab0977c69f9f1f594a720248e7db61f0ab4d555a
Spencer <spencer@360spencer.com>
2023-02-03 17:01:32 -0500

    New stuff 2

---
+++ wp-content/themes/salient/nectar/redux-framework/ReduxCore/templates/panel/content.tpl.php
@@ -0,0 +1,6 @@
+<!-- Header Block -->
+<br>
+<b>Fatal error</b>:  Uncaught Error: Using $this when not in object context in /Users/spencer/Local Sites/360spencer/app/public/wp-content/themes/salient/nectar/redux-framework/ReduxCore/templates/panel/content.tpl.php:12
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/spencer/Local Sites/360spencer/app/public/wp-content/themes/salient/nectar/redux-framework/ReduxCore/templates/panel/content.tpl.php</b> on line <b>12</b><br>
